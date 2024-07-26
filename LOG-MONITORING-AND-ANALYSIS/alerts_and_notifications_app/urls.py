91ca14e3b80a13d17a4fe230ebca0a7928774873
mark <mmarkmarkm0@gmail.com>
2024-07-26 23:51:36 +0300

    templates addition

--- LOG-MONITORING-AND-ANALYSIS/alerts_and_notifications_app/urls.py
+++ LOG-MONITORING-AND-ANALYSIS/alerts_and_notifications_app/urls.py
@@ -4,7 +4,10 @@ from . import views
 urlpatterns=[
 
     #ALERTS
+    path('alerts-config/',views.alertconfig,name='alertconfig'),
     path('alerts-history/',views.alertspage,name='alertspage'),
     path('scheduled-reports/',views.scheduledreports,name='scheduledreports'), 
     path('add-scheduled-reports/',views.addscheduledreport,name='addscheduledreport'),
+    path('custom-alerts/',views.customalerts,name='customalerts'),
+    path('not-settings/',views.notification_settings,name='notification_settings'),
 ]
--- LOG-MONITORING-AND-ANALYSIS/alerts_and_notifications_app/views.py
+++ LOG-MONITORING-AND-ANALYSIS/alerts_and_notifications_app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 #ALERTS
 
+def alertconfig(request):
+    context={}
+    return render(request,'baseapp/alertconfig/alertconfig.html',context)
+
 def alertspage(request):
     context={}
     return render(request,'baseapp/alerts/alerts.html',context)
@@ -14,3 +18,10 @@ def addscheduledreport(request):
     context={}
     return render(request,'baseapp/scheduledreports/addreport.html',context)
     
+def customalerts(request):
+    context={}
+    return render(request,'baseapp/customalerts/customalerts.html',context)
+
+def notification_settings(request):
+    context={}
+    return render(request,'baseapp/notificationsettings/notsettings.html',context)
